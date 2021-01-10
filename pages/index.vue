3eb75cca88d745433a214d34431d43ce13ea9bed
1071sh <k.nishida@viatage-llc.com>
2021-01-10 10:18:26 +0900

    section3 part70

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,65 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-posts
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div>
+            <Navbar />
+            <div class="blogs-page">
+                <div class="main-content">
+                    <div class="container">
+                        <div class="columns is-mobile">
+                            <div class="column is-8 is-offset-2">
+                                <div class="section">
+                                    <div class="title">
+                                        <h1>Newest Posts</h1>
+                                        <hr />
+                                    </div>
+                                    <PostItem
+                                        v-for="post in posts"
+                                        :key="post._id"
+                                        :title="post.title"
+                                        :subtitle="post.subtitle"
+                                        :date="post.createedAt"
+                                        :isRead="post.isRead"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {}
+import Navbar from "@/components/Navbar";
+import PostItem from "@/components/PostItem";
+export default {
+    components: {
+        Navbar,
+        PostItem
+    },
+    data() {
+        return {
+            title: "My Title From Page",
+            posts: [
+                {
+                    _id: "1",
+                    title: "My First Post",
+                    subtitle: "My First Subtitle",
+                    createedAt: new Date(),
+                    isRead: false
+                },
+                {
+                    _id: "2",
+                    title: "My Seconde Post",
+                    subtitle: "My Seconde Subtitle",
+                    createedAt: new Date(),
+                    isRead: true
+                }
+            ]
+        };
+    }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style scoped></style>
